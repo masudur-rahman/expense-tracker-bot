252cf3ceccc59f226cb763edc145ec07f63851fa
Masudur Rahman <masudjuly02@gmail.com>
2025-02-27 00:33:21 +0600

    Replace “”(iphone double quote) with ""
    Signed-off-by: Masudur Rahman <masudjuly02@gmail.com>

--- pkg/parse.go
+++ pkg/parse.go
@@ -6,6 +6,8 @@ import (
 )
 
 func SplitString(input string, sep rune) []string {
+	input = strings.ReplaceAll(input, "”", "\"")
+	input = strings.ReplaceAll(input, "“", "\"")
 	reader := csv.NewReader(strings.NewReader(input))
 	reader.Comma = sep
 	fields, _ := reader.Read()
